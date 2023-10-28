9c75b4aea20db9a80c1007e8314988a225c7fddb
Ashot0 <illia.holovan@gmail.com>
2023-10-28 22:31:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+import './styles/App.css'
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/Button/MyButton';
+import MyInput from './components/UI/Input/MyInput';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'JavaScript', body: 'Description' },
+		{ id: 2, title: 'JavaScript', body: 'Description' },
+		{ id: 3, title: 'JavaScript', body: 'Description' },
+		{ id: 4, title: 'JavaScript', body: 'Description' },
+		{ id: 5, title: 'JavaScript', body: 'Description' },
+	])
+
+	const [posts2, setPosts2] = useState([
+		{ id: 1, title: 'Python', body: 'Description' },
+		{ id: 2, title: 'Python', body: 'Description' },
+		{ id: 3, title: 'Python', body: 'Description' },
+		{ id: 4, title: 'Python', body: 'Description' },
+		{ id: 5, title: 'Python', body: 'Description' },
+	])
+
+	const [title, setTitle] = useState('')
+	const [body, setBody] = useState('')
+	// const bodyInputRef = useRef();
+
+	const addNewPost = (e) => {
+		e.preventDefault()
+		const newPost = {
+			id: Date.now(),
+			title,
+			body,
+		}
+		setPosts([...posts, newPost])
+		setTitle('')
+		setBody('')
+	}
+
+	return (
+		<div className="App">
+			<form>
+				{/* Управляемый компонент */}
+				<MyInput
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+					type="text"
+					placeholder='Название поста'
+				/>
+				{/* Неуправляемый компонент */}
+				<MyInput
+					value={body}
+					onChange={e => setBody(e.target.value)}
+					type="text"
+					placeholder='Описание поста'
+				/>
+				<MyButton onClick={addNewPost}>
+					Создать пост
+				</MyButton>
+			</form>
+			<PostList posts={posts} title="Посты про JS 1" />
+			<PostList posts={posts2} title="Посты про Python 2" />
+		</div>
+	);
 }
 
 export default App;
